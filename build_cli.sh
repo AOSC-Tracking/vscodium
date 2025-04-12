23596b2aa02cc282fe3820b86801c6ea0f2ff709
Mingcong Bai <jeffbai@aosc.io>
2025-04-12 12:49:44 +0800

    fix(build_cli.sh): drop hard-coded cross compiler setting
    ARM != cross compile.

--- build_cli.sh
+++ build_cli.sh
@@ -63,12 +63,6 @@ else
   if [[ "${VSCODE_ARCH}" == "arm64" ]]; then
     VSCODE_CLI_TARGET="aarch64-unknown-linux-gnu"
 
-    if [[ "${CI_BUILD}" != "no" ]]; then
-      export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-      export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
-      export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-      export PKG_CONFIG_ALLOW_CROSS=1
-    fi
   elif [[ "${VSCODE_ARCH}" == "armhf" ]]; then
     VSCODE_CLI_TARGET="armv7-unknown-linux-gnueabihf"
 
